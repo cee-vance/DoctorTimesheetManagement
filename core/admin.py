e204cea923f8ceaf3dd0c341b297be9f37063559
NeoVic2006 <NeoVic2006@gmail.com>
2021-09-09 11:22:27 -0500

    First data for tables

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from core.models import User, WorkEntry
 # Register your models here.
+
+
+admin.site.register(User)
+admin.site.register(WorkEntry)
--- core/models.py
+++ core/models.py
@@ -3,24 +3,24 @@ from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
-class User( models.Model):
-    user_id = IntegerField( null = False, on_delete=models.CASCADE)
-    firstName = CharField( max_length=50, null= False)
-    lastName = CharField( max_length=100, null= False)
-    description = CharField(max_length=500 , null = True)
+class User(models.Model):
+    user_id = models.IntegerField( null = False)
+    firstName = models.CharField( max_length=50, null= False)
+    lastName = models.CharField( max_length=100, null= False)
+    description = models.CharField(max_length=500 , null = True)
     # TO DO: change email field
-    email = CharField( max_length = 100, null=True)
-    login_id = CharField( max_length = 50, null = False)
-    password = CharField( max_length = 50 , null = False)
-    password_conf = CharField( max_length = 50, null = False)
+    email = models.CharField( max_length = 100, null=True)
+    login_id = models.CharField( max_length = 50, null = False)
+    password = models.CharField( max_length = 50 , null = False)
+    password_conf = models.CharField( max_length = 50, null = False)
 
 
 class WorkEntry( models.Model):
     # Foreign key for User model
-    doctor_id = ForeignKey(User, on_delete=models.CASCADE)
-    date = DateTimeField(auto_null_add=True)
-    start_time = IntegerField(null= True, default=0)
-    end_time = IntegerField(null = True, default = 0)
+    doctor_id = models.ForeignKey(User, on_delete=CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    start_time = models.IntegerField(null= True, default=0)
+    end_time = models.IntegerField(null = True, default = 0)
     # To Do:
 
 """"
