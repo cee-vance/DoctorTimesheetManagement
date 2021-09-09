429da45e0f571f4c8fed85ec5573ba3bce519c11
Collin Hunt <dev123prac@mail.com>
2021-09-09 10:53:32 -0700

    working on home page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.urls import reverse, reverse_lazy
+from django.utils import timezone
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 # Create your views here.
+
+
+#@login_required
+class admin_home(ListView):
+    model = User
+    template = 'home.html'
+    context_object_name = 'users'
