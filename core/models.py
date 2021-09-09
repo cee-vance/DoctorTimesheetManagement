0b5fcc37240f78b29b16d32ac3149d5adaf601f9
Collin Hunt <dev123prac@mail.com>
2021-09-09 12:30:56 -0700

    added sector, hours, finished workentry , location

--- core/models.py
+++ core/models.py
@@ -15,12 +15,43 @@ class User(models.Model):
     password_conf = models.CharField( max_length = 50, null = False)
 
 
+
+class HoursCode(models.Model):
+    """
+    Defines the type of hours worked
+    choices are AMCO or FPB
+    """
+    CHOICES = (('A','AMCO'), ('F','FPB'))
+    code = models.CharField(choices = CHOICES, max_length=4)
+
+
+class Sector(models.Model):
+    """
+    Divides all the locations into sectors
+    choices are east and west
+    """
+    SectorChoices =  (('E','East'), ('W','West'))
+
+    name = models.CharField(choices = SectorChoices, max_length=4 )
+
+
+class Location(models.Model):
+    """
+    Represents the physical location
+    where a WorkEntry took place
+    """
+    name = models.CharField(max_length=50, null= False)
+    sector_id = models.ForeignKey(Sector, on_delete=CASCADE)
+    address = models.CharField(max_length=150, null=True)
+
 class WorkEntry( models.Model):
     # Foreign key for User model
     doctor_id = models.ForeignKey(User, on_delete=CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     start_time = models.IntegerField(null= True, default=0)
     end_time = models.IntegerField(null = True, default = 0)
+    hourscode_id = models.ForeignKey(HoursCode, on_delete=CASCADE)
+
     # To Do:
 
 """"
@@ -28,6 +59,4 @@ TO DO:
 lOCATION TABLE, HOURSCODE TABLE
 
 """
-
-
 
