646ab30d3e65e00492775d92c05d34e642c869e4
Collin Hunt <dev123prac@mail.com>
2021-09-10 06:35:30 -0700

    Made major changes to the modelsgit add core/admin.py !

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from core.models import User, WorkEntry
+from core.models import User, WorkEntry, Location
 # Register your models here.
 
 
 admin.site.register(User)
 admin.site.register(WorkEntry)
+admin.site.register(Location)
--- core/models.py
+++ core/models.py
@@ -1,38 +1,27 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class User(models.Model):
-    user_id = models.IntegerField( null = False)
+    """"
+    The user of the Doctor Management System
+    usually a doctor
+    """
     firstName = models.CharField( max_length=50, null= False)
     lastName = models.CharField( max_length=100, null= False)
     description = models.CharField(max_length=500 , null = True)
     # TO DO: change email field
-    email = models.CharField( max_length = 100, null=True)
-    login_id = models.CharField( max_length = 50, null = False)
-    password = models.CharField( max_length = 50 , null = False)
-    password_conf = models.CharField( max_length = 50, null = False)
+    email = models.EmailField( max_length = 254, null=True)
+
+    user = models.ForeignKey(User, on_delete = CASCADE, default = 1 )
+    email_conf = models.BooleanField(default = False)
 
 
 
-class HoursCode(models.Model):
-    """
-    Defines the type of hours worked
-    choices are AMCO or FPB
-    """
-    CHOICES = (('A','AMCO'), ('F','FPB'))
-    code = models.CharField(choices = CHOICES, max_length=4, default = 'A')
 
 
-class Sector(models.Model):
-    """
-    Divides all the locations into sectors
-    choices are east and west
-    """
-    SectorChoices =  (('E','East'), ('W','West'))
-
-    name = models.CharField(choices = SectorChoices, max_length=4 )
 
 
 class Location(models.Model):
@@ -40,23 +29,24 @@ class Location(models.Model):
     Represents the physical location
     where a WorkEntry took place
     """
+    SECTOR_CHOICES = (('E', 'East'), ('W', 'West'))
     name = models.CharField(max_length=50, null= False)
-    sector_id = models.ForeignKey(Sector, on_delete=CASCADE)
+    sector = models.CharField(choices = SECTOR_CHOICES, max_length = 4,default = 'East')
     address = models.CharField(max_length=150, null=True)
 
 class WorkEntry( models.Model):
-    # Foreign key for User model
+    """
+    Represents a doctors work entry
+    including date , start, end time,
+    hours code for billing and a reference
+    to the doctor who worked
+    """
+    HC_CHOICES = (('A', 'AMCO'), ('F', 'FPB'))
     doctor_id = models.ForeignKey(User, on_delete=CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     start_time = models.IntegerField(null= True, default=0)
     end_time = models.IntegerField(null = True, default = 0)
-    hourscode_id = models.ForeignKey(HoursCode, on_delete=CASCADE, default=0)
+    hourscode = models.CharField(choices = HC_CHOICES, max_length = 4, default = 'AMCO')
 
-    # To Do:
 
-""""
-TO DO:
-lOCATION TABLE, HOURSCODE TABLE
-
-"""
 
