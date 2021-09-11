9102f4345f6b120fc7d3c6a51da36b3c723cdbf0
NeoVic2006 <NeoVic2006@gmail.com>
2021-09-11 01:56:59 -0500

    Small fixes

--- core/forms.py
+++ core/forms.py
@@ -1,10 +1,11 @@
 from django import forms
+from django.db.models.fields import DateField
 from django.forms import fields
 from core.models import User, Location, WorkEntry
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
-
+from django.forms.models import modelformset_factory
 
 
 class ReportForm(forms.ModelForm):
@@ -13,10 +14,14 @@ class ReportForm(forms.ModelForm):
         fields = '__all__'
 
 
+
+
 class StempForm(forms.ModelForm):
     class Meta:
         model = WorkEntry
         fields = '__all__'
+    
+StempFormSet = modelformset_factory(WorkEntry, fields=('start_time', 'end_time'), extra=8,)
 
 
 class DoctorCreateForm(forms.ModelForm):
--- core/views.py
+++ core/views.py
@@ -3,9 +3,9 @@ from django.urls import reverse, reverse_lazy
 from django.utils import timezone
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import User, Location, WorkEntry
-from .forms import ReportForm, StempForm
+from .forms import ReportForm, StempForm, StempFormSet
 # Create your views here.
-
+from django.forms import formset_factory
 
 #@login_required
 class admin_main(ListView):
@@ -37,11 +37,7 @@ class stamps_page(CreateView): # CreateView, Model.FormSetFactory 10-15 instance
     model = WorkEntry
     template_name = 'stamps.html'
     form_class = StempForm
-    
-    modelformset_factory = WorkEntry
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({'workentries': WorkEntry.objects.all})
-        return context
+    #modelformset_factory = WorkEntry
+    success_url = reverse_lazy('core:stamps')
 
+    ArticleFormSet = formset_factory(StempForm)
