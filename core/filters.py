fc0d4dcb150f6968c4d8408740e87900939a1bac
NeoVic2006 <NeoVic2006@gmail.com>
2021-09-14 08:52:46 -0500

    Requirements update

--- core/filters.py
+++ core/filters.py
@@ -2,6 +2,7 @@ import django_filters
 from .models import *
 from django_filters import DateFilter
 
+
 class WorkEntryFilter(django_filters.FilterSet):
     """
     Filters WorkEntry by user , date range
